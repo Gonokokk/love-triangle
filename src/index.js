ba589b39e4eb746dcd232666d0802cc4af4b365f
Aleh <32686077+Gonokokk@users.noreply.github.com>
2018-02-22 19:37:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,29 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+ 
+  let trianglesCounter = 0;
+  let spichoneesCount = preferences.length;
+  let triangleEdges = [];
+ 
+  for (let spichoneeIndex = 1; spichoneeIndex <= spichoneesCount; spichoneeIndex++) {
+     if (triangleEdges.indexOf(spichoneeIndex) >= 0) {
+     continue;
+    }
+    let triangle = [];
+    let currentSpichonee = spichoneeIndex;
+    for (let edgeCnt = 0; edgeCnt < 3; edgeCnt++) {
+      if (triangle.indexOf(currentSpichonee) >= 0) {
+        currentSpichonee = -1;
+        break;
+      }
+      triangle.push(currentSpichonee);
+      currentSpichonee = preferences[currentSpichonee-1];
+    }
+      if (currentSpichonee === spichoneeIndex) {
+        trianglesCounter++;
+        triangleEdges = triangleEdges.concat(triangle);
+      }
+    }
+   return trianglesCounter;
 };
